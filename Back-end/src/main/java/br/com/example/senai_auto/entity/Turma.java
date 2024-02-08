fecf8e60a5dbe59394725f762d84dc26752e79c3
Lucas Oliveira <134160637+lucasoli274@users.noreply.github.com>
2024-02-08 17:19:53 -0300

    Update Turma.java

--- Back-end/src/main/java/br/com/example/senai_auto/entity/Turma.java
+++ Back-end/src/main/java/br/com/example/senai_auto/entity/Turma.java
@@ -2,10 +2,7 @@ package br.com.example.senai_auto.entity;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
